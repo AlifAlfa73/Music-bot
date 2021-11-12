c4b3be489a91e0463c3b1e3d29d21122cb0b1774
IqrarAminullah <Iqrar.silveria@gmail.com>
2021-11-12 13:27:59 +0700

    fixes help :jerry:

--- commands/core/help.js
+++ commands/core/help.js
@@ -13,11 +13,11 @@ module.exports = {
         embed.setAuthor(client.user.username, client.user.displayAvatarURL({ size: 1024, dynamic: true }));
 
         const commands = client.commands.filter(x => x.showHelp !== false);
-        const desc = 'This code comes from a GitHub project [ZerioDev/Music-bot](https://github.com/ZerioDev/Music-bot).\nThe use of this one is possible while keeping the credits for free.\nIf you want to remove the credits join the Discord support server.\n'
+        var desc = 'This code comes from a GitHub project [ZerioDev/Music-bot](https://github.com/ZerioDev/Music-bot).\nThe use of this one is possible while keeping the credits for free.\nIf you want to remove the credits join the Discord support server.\n\n'
         desc = desc + '**Supports** : Youtube Track, Youtube Playlist, Spotify Track, Spotify Playlist, Soundcloud Track\n';
         desc = desc + '**Not Supported** : Soundcloud Playlist';
 
-        embed.setDescription('desc');
+        embed.setDescription(desc);
         embed.addField(`Enabled - ${commands.size}`, commands.map(x => `\`${x.name}${x.aliases[0] ? ` (${x.aliases.map(y => y).join(', ')})\`` : '\`'}`).join(' | '));
 
         embed.setTimestamp();
