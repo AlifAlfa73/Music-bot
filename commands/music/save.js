b121797eb8c2a98cc10c680cda6820ea2682bdac
IqrarAminullah <Iqrar.silveria@gmail.com>
2021-11-12 15:39:33 +0700

    fix save

--- commands/music/save.js
+++ commands/music/save.js
@@ -3,7 +3,7 @@ const playlistUtils = require('./musicutils/playlistutils')
 function saveSong(message, queue){
     message.author.send(`You saved the track ${queue.current.title} | ${queue.current.author} from the server ${message.guild.name} ✅`).then(() => {
         message.channel.send(`I have sent you the title of the music by private messages ✅`);
-    }).catch(()=> {
+    }).catch(error => {
         console.error(error)
         message.channel.send(`Unable to send you a private message ${message.author}... try again ? ❌`);
     });
