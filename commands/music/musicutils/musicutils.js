b49bfd1b9a0912abe585e610647caa0b075ed95f
IqrarAminullah <Iqrar.silveria@gmail.com>
2021-11-12 10:40:22 +0700

    Streamlining loop mode change

--- commands/music/musicutils/musicutils.js
+++ commands/music/musicutils/musicutils.js
@@ -1,5 +1,6 @@
 
 const { QueryType } = require('discord-player');
+const { QueueRepeatMode, Queue } = require('discord-player');
 
 module.exports.search = async function(message,param){ 
         const res = await player.search(param, {
@@ -13,7 +14,6 @@ module.exports.search = async function(message,param){
         return res;
 }
 
-
 module.exports.createQueue = async function(player, message){
     const queue = await player.createQueue(message.guild, {
         ytdlOptions: {
@@ -26,6 +26,60 @@ module.exports.createQueue = async function(player, message){
     });
 
     return queue;
+}
+
+module.exports.setLoopQueue = async function(queue){
+    var success = false;
+    switch (queue.repeatMode){
+        case 0:
+            success = queue.setRepeatMode(QueueRepeatMode.QUEUE);
+            break;
+        case 1:
+            queue.setRepeatMode(QueueRepeatMode.OFF);
+            success = queue.setRepeatMode(QueueRepeatMode.QUEUE);
+            break;
+        case 2:
+            success = true;
+            break;
+        default:
+            success = false;
+    }
+    return success;
+}
+
+module.exports.setLoopTrack = async function(queue){
+    var success = false;
+    switch (queue.repeatMode){
+        case 0:
+            success = queue.setRepeatMode(QueueRepeatMode.TRACK);
+            break;
+        case 1:
+            success = true;
+            break;
+        case 2:
+            queue.setRepeatMode(QueueRepeatMode.OFF);
+            success = queue.setRepeatMode(QueueRepeatMode.TRACK);
+            break;
+        default:
+            success = false;
+    }
+    return success;
+}
+
+module.exports.setLoop = async function(queue, loopType){
+    var success = false;
+    switch(loopType){
+        case 'queue':
+            success = this.setLoopQueue(queue);
+            break;
+        case 'song':
+            success = this.setLoopTrack(queue);
+            break;
+        case 'off':
+            success = queue.setRepeatMode(QueueRepeatMode.OFF);
+            break;
+    }
+    return success;
 }
 
 module.exports.voiceConnect = async function(message, queue){
