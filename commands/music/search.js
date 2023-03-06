21e6412fed0deaf52f3e20dd2b1f0cb168a48c44
IqrarAminullah <Iqrar.silveria@gmail.com>
2023-03-06 10:28:11 +0700

    fixed search command

--- commands/music/search.js
+++ commands/music/search.js
@@ -1,38 +1,46 @@
-const { ApplicationCommandOptionType } = require('discord.js');
 const { QueryType } = require('discord-player');
-
+const { ApplicationCommandOptionType } = require('discord.js');
 module.exports = {
-    name: 'playsearch',
-    aliases: ['psh'],
-    utilisation: '{prefix}search [song name]',
+    name: 'search',
+    description: "Search youtube for a song",
     voiceChannel: true,
     options: [
         {
             name: 'song',
-            description: 'the song you want to search',
+            description: 'the song you want to play',
             type: ApplicationCommandOptionType.String,
             required: true,
         }
     ],
 
     async execute({ inter }) {
+	await inter.deferReply();
         const song = inter.options.getString('song');
-
         const res = await player.search(song, {
             requestedBy: inter.member,
             searchEngine: QueryType.AUTO
         });
 
-        if (!res || !res.tracks.length) return inter.reply({ content: `No results found ${inter.member}... try again ? ❌`, ephemeral: true });
+        if (!res || !res.tracks.length) return inter.editReply({ content: `No results found ${inter.member}... try again ? ❌`, ephemeral: true });
 
-        return inter.reply({content: `${res.tracks[0]}`})
+        //const queue = await player.createQueue(inter.guild, {
+        //    metadata: inter.channel,
+        //    spotifyBridge: client.config.opt.spotifyBridge,
+        //    initialVolume: client.config.opt.defaultvolume,
+        //    leaveOnEnd: client.config.opt.leaveOnEnd
+        //});
 
-        // collector.on('collect', async (query) => {
-        //     if (query.content.toLowerCase() === 'cancel') return inter.followUp({ content: `Search cancelled ✅`, ephemeral: true }), collector.stop();
-        // });
+        //try {
+        //    if (!queue.connection) await queue.connect(inter.member.voice.channel);
+        //} catch {
+        //    await player.deleteQueue(inter.guildId);
+        //    return inter.editReply({ content: `I can't join the voice channel ${inter.member}... try again ? ❌`, ephemeral: true});
+        //}
 
-        // collector.on('end', (msg, reason) => {
-        //     if (reason === 'time') return inter.followUp({ content:`Search timed out ${inter.member}... try again ? ❌`, ephemeral: true })
-        // });
+       await inter.editReply({ content:`${res.tracks[0].url}`});
+
+        //res.playlist ? queue.addTracks(res.tracks) : queue.addTrack(res.tracks[0]);
+
+        //if (!queue.playing) await queue.play();
     },
 };
